1db2d49ddf92268b77be1b54b9a09f0e5132e7fa
Prasath B <prasathb8506@gmail.com>
2026-08-27 11:05:26 +0530

    Add function to visualize network states
    This function visualizes three states of a network: the original, a digital twin with faulty nodes highlighted, and the optimized version.

---
+++ matlab/visualize_network.m
@@ -0,0 +1,25 @@
+```matlab
+function visualize_network(original, twin, optimized, faultyNodes)
+% Displays 3 states of the network with faulty node highlights
+
+figure;
+
+subplot(1,3,1);
+G1 = graph(original.adjMatrix);
+plot(G1, 'NodeLabel', 1:length(original.nodes));
+title('Original Network');
+
+subplot(1,3,2);
+G2 = graph(twin.adjMatrix);
+p = plot(G2, 'NodeLabel', 1:length(twin.nodes));
+highlight(p, faultyNodes, 'NodeColor','r');
+title('Digital Twin (with Faults)');
+
+subplot(1,3,3);
+G3 = graph(optimized.adjMatrix);
+q = plot(G3, 'NodeLabel', 1:length(optimized.nodes));
+highlight(q, faultyNodes, 'NodeColor','g');
+title('Optimized Network');
+
+end
+```
